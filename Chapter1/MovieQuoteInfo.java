3ce83cc1b05a2e401f5e05f3109b2b86822dd320
Abiera <147354935+Abiera@users.noreply.github.com>
2024-08-15 18:32:13 +0800

    Add files via upload

---
+++ Chapter1/MovieQuoteInfo.java
@@ -0,0 +1,16 @@
+public class MovieQuoteInfo {
+    
+    public static void main(String[] args) {
+        
+        String quote = "Maybe i am prideful";
+        String movie = "HACKSAW RIDGE";
+        String character = "Desmond T. Doss";
+        int year = 2016;
+
+        
+        System.out.println("Quote: \"" + quote + "\"");
+        System.out.println("Movie: " + movie);
+        System.out.println("Character: " + character);
+        System.out.println("Year: " + year);
+    }
+}
