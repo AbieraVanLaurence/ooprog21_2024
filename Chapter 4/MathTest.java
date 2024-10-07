c5dc0e2c7acc01c8202457e1386f43a0f0f596ea
ABIERA , VR <147354935+AbieraVanLaurence@users.noreply.github.com>
2024-10-07 08:50:14 +0800

    Update MathTest.java

--- Chapter 4/MathTest.java
+++ Chapter 4/MathTest.java
@@ -1,27 +1,14 @@
-public class MathTest {
+ public class MathTest{
     public static void main(String[] args) {
-        
-        double valueForSqrt = 37;
-        double valueForSin = -Math.PI / 2;
-        double valueForCos = Math.PI;
-        double floorValue = 22.5;
-        double ceilValue = 22.5;
-        double roundValue = 22.5;
-        int maxValue1 = 68;
-        int maxValue2 = 71;
-        int minValue1 = 68;
-        int minValue2 = 71;
-        double randomValue = Math.random() * 100; 
-         
-       
-        System.out.println("SQRT: " + Math.sqrt(valueForSqrt));
-        System.out.println("SIN: " + Math.sin(valueForSin));
-        System.out.println("COS: " + Math.cos(valueForCos));
-        System.out.println("FLOOR: " + Math.floor(floorValue) + 
-                           " CEIL: " + Math.ceil(ceilValue) + 
-                           " ROUND: " + Math.round(roundValue));
-        System.out.println("MAX: " + Math.max(maxValue1, maxValue2));
-        System.out.println("MIN: " + Math.min(minValue1, minValue2));
-        System.out.println("RANDOM: " + randomValue);
+        double value = 20;
+        System.out.println("SQRT: " + Math.sqrt(value));
+        System.out.println("SIN: " + Math.sin(value));
+        System.out.println("COS: " + Math.cos(value));
+        System.out.println("FLOOR: " + Math.floor(value));
+        System.out.println("CEIL: " + Math.ceil(value));
+        System.out.println("ROUND: " + Math.round(value));
+        System.out.println("MAX: " + Math.max(80, 50));
+        System.out.println("MIN: " + Math.min(80, 50));
+        System.out.println("RANDOM: " + Math.random() * 100);
     }
 }
